2c5d12c6fa4aa0e0507d0a97ef5066167aebc987
Arianna <ariigrangetto5@gmail.com>
2025-07-09 20:13:26 -0300

    toSorted dates and times

--- src/components/ListOfAppointments.tsx
+++ src/components/ListOfAppointments.tsx
@@ -10,9 +10,23 @@ export function WithoutAppointments() {
 export default function ListOfAppointments() {
   const appointments = useAppointmentStore((state) => state.appointment);
 
+  const sortedAppointments = (() => {
+    return appointments.toSorted((a, b) => {
+      const dateA = a.date ?? "";
+      const dateB = b.date ?? "";
+      const timeA = a.time ?? "";
+      const timeB = a.time ?? "";
+
+      const fullDateA = new Date(`${dateA}T${timeA}`);
+      const fullDateB = new Date(`${dateB}T${timeB}`);
+
+      return fullDateA.getTime() - fullDateB.getTime();
+    });
+  })();
+
   const hasAppointments = appointments.length > 0;
   return hasAppointments ? (
-    <InfoAppointments appointments={appointments} />
+    <InfoAppointments appointments={sortedAppointments} />
   ) : (
     <WithoutAppointments />
   );
--- src/types.d.ts
+++ src/types.d.ts
@@ -6,3 +6,9 @@ export interface Appointments {
   time?: string;
   date?: string;
 }
+
+declare global {
+  interface Array<T> {
+    toSorted(compareFn?: (a: T, b: T) => number): T[];
+  }
+}
