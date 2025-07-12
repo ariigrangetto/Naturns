651d9be697fdb192e751396e0c3f0160165405d7
Arianna <ariigrangetto5@gmail.com>
2025-07-12 20:16:59 -0300

    icon

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import Turnos from "./components/Turnos";
 function App() {
   return (
     <>
+      <img
+        src='/icon.png'
+        alt='APP ICON'
+        className='h-[120px] flex justify-center m-auto rounded-3xl mb-2'
+      />
       <header>
-        <img
-          src='/icon.png'
-          alt='APP ICON'
-          className='h-[120px] flex justify-center m-auto rounded-3xl mb-2'
-        />
         <h1 className='font-sans text-4xl font-semibold text-black '>
           Appointment scheduler:
         </h1>
