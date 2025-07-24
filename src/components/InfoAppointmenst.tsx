711fa4f7d7dc5529f22045742bbe35f1ddabdca4
Arianna <ariigrangetto5@gmail.com>
2025-07-24 17:19:14 -0300

    validations

--- src/components/InfoAppointmenst.tsx
+++ src/components/InfoAppointmenst.tsx
@@ -1,6 +1,7 @@
 import { Calendar1, CircleX, Clock2 } from "lucide-react";
 import { useAppointmentStore } from "../Store/useAppointmentStore";
 import type { Appointments } from "../types.d";
+import { motion } from "framer-motion";
 
 /* eslint-disable react/react-in-jsx-scope */
 interface InfoAppointmentsPros {
@@ -22,39 +23,45 @@ export function InfoAppointments({ appointments }: InfoAppointmentsPros) {
         <strong className='ml-2'>{appointments.length}</strong>
       </h2>
 
-      <ul className='mt-5 flex flex-col gap-4'>
-        {appointments.map((item) => {
-          return (
-            <li
-              key={item.id}
-              className='bg-white shadow-md rounded-2xl p-4 border border-gray-200'
-            >
-              <p className='text-lg font-semibold text-black'>
-                <p className='font-bold items-center flex justify-center m-auto'>
-                  Client: <p className='font-semibold ml-1'> {item.name}</p>
-                </p>
-              </p>
-              <p className='text-sm text-gray-600'>{item.section}</p>
-              <p className='text-sm text-gray-600'>{item.description}</p>
-              <p className='text-sm text-center items-center text-gray-600 justify-center flex m-auto'>
-                <Clock2 size={15} className=' mr-1' />
-                {item.time}
-              </p>
-              <p className='text-sm text-gray-600 items-center justify-center flex m-auto'>
-                <Calendar1 size={15} className='mr-1' />
-                {item.date}
-              </p>
-              <button
-                onClick={() => handleDelete(item.id)}
-                className='mt-3 bg-[rgba(255,197,183,0.69)] hover:bg-[rgb(255,202,189)] transition-all text-black px-4 py-1 rounded-full font-medium text-center flex justify-center m-auto items-center'
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.2 }}
+      >
+        <ul className='mt-5 flex flex-col gap-4'>
+          {appointments.map((item) => {
+            return (
+              <li
+                key={item.id}
+                className='bg-white shadow-md rounded-2xl p-4 border border-gray-200'
               >
-                <CircleX size={15} className='mr-1' />
-                <p className=''>Delete</p>
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+                <p className='text-lg font-semibold text-black'>
+                  <p className='font-bold items-center flex justify-center m-auto'>
+                    Client: <p className='font-semibold ml-1'> {item.name}</p>
+                  </p>
+                </p>
+                <p className='text-sm text-gray-600'>{item.section}</p>
+                <p className='text-sm text-gray-600'>{item.description}</p>
+                <p className='text-sm text-center items-center text-gray-600 justify-center flex m-auto'>
+                  <Clock2 size={15} className=' mr-1' />
+                  {item.time}
+                </p>
+                <p className='text-sm text-gray-600 items-center justify-center flex m-auto'>
+                  <Calendar1 size={15} className='mr-1' />
+                  {item.date}
+                </p>
+                <button
+                  onClick={() => handleDelete(item.id)}
+                  className='mt-3 bg-[rgba(255,197,183,0.69)] hover:bg-[rgb(255,202,189)] transition-all text-black px-4 py-1 rounded-full font-medium text-center flex justify-center m-auto items-center'
+                >
+                  <CircleX size={15} className='mr-1' />
+                  <p className=''>Delete</p>
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </motion.div>
     </>
   );
 }
--- src/components/Turnos.tsx
+++ src/components/Turnos.tsx
@@ -2,6 +2,7 @@ import { useId, useState } from "react";
 import { useAppointmentStore } from "../Store/useAppointmentStore";
 import type { Appointments } from "../types.d";
 import { MousePointerClick } from "lucide-react";
+import { motion } from "framer-motion";
 
 /* eslint-disable react/react-in-jsx-scope */
 export default function Turnos() {
@@ -11,6 +12,13 @@ export default function Turnos() {
   const [time, setTime] = useState<string>("");
   const [date, setDate] = useState<string>("");
   const [section, setSection] = useState<string>("");
+  const [errors, setErrors] = useState({
+    name: false,
+    description: false,
+    time: false,
+    date: false,
+    section: false,
+  });
 
   const saveAppointment = useAppointmentStore((state) => state.saveAppointment);
 
@@ -25,6 +33,20 @@ export default function Turnos() {
   };
 
   const handleClick = () => {
+    const newErrors = {
+      name: name === "",
+      description: description === "",
+      time: time === "",
+      date: date === "",
+      section: section === "",
+    };
+
+    const hasErrors = Object.values(newErrors).some(Boolean);
+
+    setErrors(newErrors);
+
+    if (hasErrors) return;
+
     const newAppointment: Appointments = {
       id: crypto.randomUUID(),
       name,
@@ -33,6 +55,7 @@ export default function Turnos() {
       date,
       section,
     };
+
     saveAppointment(newAppointment);
 
     setName("");
@@ -42,16 +65,24 @@ export default function Turnos() {
     setTime("");
 
     setShowForm(false);
+    setErrors({
+      name: false,
+      description: false,
+      time: false,
+      date: false,
+      section: false,
+    });
   };
 
   const toggleForm = () => {
     setShowForm(!showForm);
   };
+
   return (
     <>
       <button
         onClick={toggleForm}
-        className='bg-[rgba(255,197,183,0.77)] py-2 px-5 mt-5 rounded-[100px] font-medium text-black hover:bg-purple-300 transition-all ease-in  '
+        className='bg-[rgba(255,197,183,0.77)] py-2 px-5 mt-5 rounded-[100px] font-medium text- transition-all ease-in  '
       >
         <p className='justify-center item-center flex'>
           New appointment
@@ -59,84 +90,113 @@ export default function Turnos() {
         </p>
       </button>
       {showForm && (
-        <div className=' bg-gray-100 mt-5 rounded-2xl p-4 shadow-md mb-5 max-w-sm'>
-          <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
-            <label htmlFor={nameId} className='text-black font-medium'>
-              Client name:
-            </label>
-            <input
-              className='bg-white px-3 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[rgba(255,197,183,0.77)]'
-              type='text'
-              id={nameId}
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.3 }}
+        >
+          <div className=' bg-gray-100 mt-5 rounded-2xl p-4 shadow-md mb-5 max-w-sm'>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+              <label htmlFor={nameId} className='text-black font-medium'>
+                Client name:
+              </label>
+              <input
+                className='bg-white px-3 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[rgba(255,197,183,0.77)]'
+                type='text'
+                id={nameId}
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              {errors.name && (
+                <p className='text-red-500 text-sm'>Please enter a name</p>
+              )}
 
-            <label htmlFor={sectionId} className='text-black font-medium'>
-              Section:
-            </label>
-            <select
-              name='select section'
-              id={sectionId}
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-                setSection(e.target.value)
-              }
-              className='bg-white px-3 py-2 rounded-xl border-gray-300focus:outline-none focus:ring-2 focus:ring-[rgba(255,197,183,0.77)]'
-              value={section}
+              <label htmlFor={sectionId} className='text-black font-medium'>
+                Section:
+              </label>
+              <select
+                name='select section'
+                id={sectionId}
+                required
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  setSection(e.target.value)
+                }
+                className='bg-white px-3 py-2 rounded-xl border-gray-300focus:outline-none focus:ring-2 focus:ring-[rgba(255,197,183,0.77)]'
+                value={section}
+              >
+                <option value='' disabled>
+                  Chosee a section
+                </option>
+                <option value='Nails'>Nails</option>
+                <option value='Hair'>Hair</option>
+              </select>
+              {errors.section && (
+                <p className='text-red-500 text-sm'>Please enter a section</p>
+              )}
+
+              <label htmlFor={descriptionId} className='text-black font-medium'>
+                Description:
+              </label>
+              <input
+                type='text'
+                id={descriptionId}
+                required
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className='bg-white px-3 py-2 rounded-xl border-gray-300 focus:outline-none focus:ring-2 focus:ring-[rgba(255,197,183,0.77)]'
+              />
+              {errors.description && (
+                <p className='text-red-500 text-sm'>
+                  Please enter a description
+                </p>
+              )}
+
+              <label htmlFor={timeId} className='text-black font-medium'>
+                Time:
+              </label>
+              <input
+                type='time'
+                id={timeId}
+                required
+                value={time}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setTime(e.target.value)
+                }
+                className='bg-white px-3 py-2 rounded-xl border-gray-300 focus:outline-none focus:ring-2 focus:ring-[rgba(255,197,183,0.77)]'
+              />
+              {errors.time && (
+                <p className='text-red-500 text-sm'>Please enter a time</p>
+              )}
+
+              <label htmlFor={dateId} className='text-black font-medium'>
+                Date:
+              </label>
+              <input
+                type='date'
+                id={dateId}
+                value={date}
+                required
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDate(e.target.value)
+                }
+                className='bg-white px-3 py-2 rounded-xl border-gray-300 focus:outline-none focus:ring-2 focus:ring-[rgba(255,197,183,0.77)]'
+              />
+              {errors.date && (
+                <p className='text-red-500 text-sm'>Please enter a date</p>
+              )}
+            </form>
+
+            <button
+              type='submit'
+              onClick={handleClick}
+              className='bg-[rgba(255,197,183,0.77)] mt-3 py-2 px-4 rounded-full font-medium text-black  transition-all ease-in'
             >
-              <option value='' disabled>
-                Chosee a section
-              </option>
-              <option value='Nails'>Nails</option>
-              <option value='Hair'>Hair</option>
-            </select>
-
-            <label htmlFor={descriptionId} className='text-black font-medium'>
-              Description:
-            </label>
-            <input
-              type='text'
-              id={descriptionId}
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className='bg-white px-3 py-2 rounded-xl border-gray-300 focus:outline-none focus:ring-2 focus:ring-[rgba(255,197,183,0.77)]'
-            />
-
-            <label htmlFor={timeId} className='text-black font-medium'>
-              Time:
-            </label>
-            <input
-              type='time'
-              id={timeId}
-              value={time}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setTime(e.target.value)
-              }
-              className='bg-white px-3 py-2 rounded-xl border-gray-300 focus:outline-none focus:ring-2 focus:ring-[rgba(255,197,183,0.77)]'
-            />
-
-            <label htmlFor={dateId} className='text-black font-medium'>
-              Date:
-            </label>
-            <input
-              type='date'
-              id={dateId}
-              value={date}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setDate(e.target.value)
-              }
-              className='bg-white px-3 py-2 rounded-xl border-gray-300 focus:outline-none focus:ring-2 focus:ring-[rgba(255,197,183,0.77)]'
-            />
-          </form>
-
-          <button
-            type='submit'
-            onClick={handleClick}
-            className='bg-[rgba(255,197,183,0.77)] mt-3 py-2 px-4 rounded-full font-medium text-black hover:bg-purple-300 transition-all ease-in'
-          >
-            Save appoitment
-          </button>
-        </div>
+              Save appoitment
+            </button>
+          </div>
+        </motion.div>
       )}
     </>
   );
